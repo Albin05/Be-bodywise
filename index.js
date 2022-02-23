26d295c89c4153d421886de145aa8d5d7e1a2ad9
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-02-23 11:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+
+import navbar from "./components/navbar.js";
+
 import footer from "./components/navbar.js";
+
+
+let navbar_div = document.getElementById("nav_container");
+navbar_div.innerHTML = navbar();
 
 let footer_content = document.getElementById("foot_cont");
 footer_content.innerHTML = footer();
