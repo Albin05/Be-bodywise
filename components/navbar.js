b5c9e32987726704384ac3c082a8f83a6abbfa7b
dishant-baraily <dishantbaraily@gmail.com>
2022-02-23 11:18:31 +0530

    Dishant

---
+++ components/navbar.js
@@ -0,0 +1,40 @@
+function navbar(){
+    return `    <div id="nav_body">
+    <a href=""> <sup id="nav_be">BE</sup> Bodywise <sup id="nav_tm">TM</sup></a>
+</div>
+<div id="nav_txt">
+    <a href="">Book Doctor Consultation</a>
+</div>
+<div id="nav_txt">
+    <a href="">Take Wellness Assessment</a>
+</div>
+<div>
+    <select id="nav_opt">
+        <option value="">Chose Concern</option>
+        <option value="">Hair</option>
+        <option value="">Skin</option>
+        <option value="">Nutrition</option>
+        <option value="">PCOS</option>
+        <option value="">Intimate Care</option>
+    </select>
+</div>
+<div id="nav_txt">
+    <a href="">All Products</a>
+</div>
+<div id="nav_txt">
+    <a href="">Refer & Earn</a>
+</div>
+<div id="nav_search">
+    <button><img src="https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_48/v1603950724/staging/Home/Images/u_search.svg" alt=""></button>
+</div>
+<div id="nav_btn">
+    <button><img src="https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_48/v1603950724/staging/Home/Images/u_user.svg" alt=""></button>
+</div>
+<div id="nav_btn">
+    <button><a href="https://wa.me/917710096671?text=Hey%21+I+would+like+to+know+more+about+Bodywise." target="_blank"><img src="https://res.cloudinary.com/mosaic-wellness/image/upload/f_auto,w_48/v1603950724/staging/Home/Images/WhastApp.svg" alt=""></a></button>
+</div>
+<div id="nav_btn">
+    <button><img src="https://res.cloudinary.com/mosaic-wellness/image/upload/v1603950724/staging/Home/Images/u_shopping-bag.svg" alt=""></button>
+</div>`;
+}
+export default navbar;
