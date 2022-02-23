a57e80a0850b1da1393b4fd0c45291e6764b611a
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-02-23 11:48:04 +0530

    Or

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
-import navbar from "./components/navbar.js";
+import {navbar, footer} from "./components/navbar.js";
 
-import footer from "./components/navbar.js";
+
 
 
 let navbar_div = document.getElementById("nav_container");
