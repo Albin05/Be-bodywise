13f49049a2fe929002a22eaa8e34ebbb5d64a8d4
sarikasingh30 <97461783+sarikasingh30@users.noreply.github.com>
2022-02-25 19:50:39 +0530

    Merge Slider one

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ navbar_div.innerHTML = navbar();
 
 let footer_content = document.getElementById("foot_cont");
 footer_content.innerHTML = footer();
+
+// Sliders Hair Skin Uti
+
+const productContainers = [...document.querySelectorAll('.sproduct-container')];
+const nxtBtn = [...document.querySelectorAll('.snxt-btn')];
+const preBtn = [...document.querySelectorAll('.spre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+});
