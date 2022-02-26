8cdb4346ae4711ed00a701d57a2528d8fd6535f6
sarikasingh30 <97461783+sarikasingh30@users.noreply.github.com>
2022-02-26 12:02:47 +0530

    Merging

--- index.js
+++ index.js
@@ -26,3 +26,40 @@ productContainers.forEach((item, i) => {
         item.scrollLeft -= containerWidth;
     })
 });
+
+
+// Doctor Health
+const dhproductContainers = [...document.querySelectorAll('.dhproduct-container')];
+const dhnxtBtn = [...document.querySelectorAll('.dhnxt-btn')];
+const dhpreBtn = [...document.querySelectorAll('.dhpre-btn')];
+
+dhproductContainers.forEach((dhitem, i) => {
+    let containerDimensions = dhitem.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    dhnxtBtn[i].addEventListener('click', () => {
+        dhitem.scrollLeft += containerWidth;
+    })
+
+    dhpreBtn[i].addEventListener('click', () => {
+        dhitem.scrollLeft -= containerWidth;
+    })
+})
+
+// Doctor experiences
+const deproductContainers = [...document.querySelectorAll('.deproduct-container')];
+const denxtBtn = [...document.querySelectorAll('.denxt-btn')];
+const depreBtn = [...document.querySelectorAll('.depre-btn')];
+
+deproductContainers.forEach((deitem, i) => {
+    let containerDimensions = deitem.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    denxtBtn[i].addEventListener('click', () => {
+        deitem.scrollLeft += containerWidth;
+    })
+
+    depreBtn[i].addEventListener('click', () => {
+        deitem.scrollLeft -= containerWidth;
+    })
+})
