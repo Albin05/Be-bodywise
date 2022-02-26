fa6d7708624841429f3a3f84ec28d963c0f0e705
Priyanshu Anany <84434172+priyanshuanany@users.noreply.github.com>
2022-02-26 15:14:15 +0530

    Signup & refer part

--- scripts/otp.js
+++ scripts/otp.js
@@ -2,7 +2,7 @@ function submit(){
     let input = document.querySelector("input").value;
 
     if(input== 1234 || input ==5678 || input == 9011){
-        window.location.href="index.html";
+        window.location.href="signup.html";
     }else{
         document.getElementById("res").textContent="Wrong OTP";
     }
